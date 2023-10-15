968691f0b4b329ac02c7a3ee09980f2497b82bca
Maryna-Korbet <97749149+Maryna-Korbet@users.noreply.github.com>
2023-10-15 07:57:19 +0300

    added task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,12 @@
+const categories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(category => {
+
+    const categoryName = category.querySelector('h2');
+    console.log(`Category: ${categoryName.textContent}`);
+
+    const categoryItem = category.querySelectorAll('li');
+    console.log(`Elements: ${categoryItem.length}`);
+});
 
