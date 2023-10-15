119cc9f893d25c7a255cc51b6863c60862b6ac0a
Maryna-Korbet <97749149+Maryna-Korbet@users.noreply.github.com>
2023-10-15 08:23:34 +0300

    added task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById("ingredients");
+
+function createMarkup(arr) {
+  const markup = arr.map(ingredient => {
+    const li = document.createElement('li');
+    li.textContent = ingredient;
+    li.classList.add("item");
+    return li;
+  })
+
+  ingredientsList.append(...markup);
+}
+
+createMarkup(ingredients);
